b7e23a63710dc3ef0a99d97742ed26802f02dc9d
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-09-16 01:12:53 +0700

    làm f

---
+++ CSS/Colors.cs
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1 style="background-color:Tomato;">Ninh Khanh Ly</h1>
+<h1 style="background-color:Orange;">2021050425</h1>
+<h1 style="background-color:DodgerBlue;">DodgerBlue</h1>
+<h1 style="background-color:MediumSeaGreen;">MediumSeaGreen</h1>
+<h1 style="background-color:Gray;">Gray</h1>
+<h1 style="background-color:SlateBlue;">SlateBlue</h1>
+<h1 style="background-color:Violet;">Violet</h1>
+<h1 style="background-color:LightGray;">LightGray</h1>
+
+</body>
+</html>
+
+
