8040825e3258a4f614da67349b3d47731bd88294
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-09-16 01:09:22 +0700

    làm c

---
+++ CSS/Selectors.cs
@@ -0,0 +1,18 @@
+< !DOCTYPE html >
+< html >
+< head >
+< style >
+p {
+    text - align: center;
+color: red;
+}
+</ style >
+</ head >
+< body >
+
+< p > Ninh Khanh Ly 2021050425</>
+<p id="para1">Me too!</p>
+<p>And me!</p>
+
+</body>
+</html>
