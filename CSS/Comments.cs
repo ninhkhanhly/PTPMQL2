552975162bc1cd6acf666af3b54fd929e87aff60
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-09-16 01:11:34 +0700

    làm e

---
+++ CSS/Comments.cs
@@ -0,0 +1,20 @@
+< !DOCTYPE html >
+< html >
+< head >
+< style >
+/* This is a single-line comment */
+p {
+color: red;
+}
+</ style >
+</ head >
+< body >
+
+< p > Hello World! </ p >
+< p > Ninh Khanh Ly 2021050425</>
+<p>CSS comments are not shown in the output.</p>
+
+</body>
+</html>
+
+
