d05914b1e0223c8ffedaad24d35b68ddbb17899d
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-09-16 01:08:18 +0700

    làm b

---
+++ CSS/Syntax.cs
@@ -0,0 +1,17 @@
+< !DOCTYPE html >
+< html >
+< head >
+< style >
+p {
+color: red;
+    text - align: center;
+}
+</ style >
+</ head >
+< body >
+
+< p > Ninh Khanh Ly 2021050425</>
+<p>These paragraphs are styled with CSS.</p>
+
+</body>
+</html>
