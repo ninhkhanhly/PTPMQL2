e3583674130f8c17e5060f62d62e9abb29bc480e
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-09-16 01:13:55 +0700

    làm g

---
+++ CSS/Backgrounds.cs
@@ -0,0 +1,19 @@
+< !DOCTYPE html >
+< html >
+< head >
+< style >
+body {
+    background - color: lightblue;
+}
+</ style >
+</ head >
+< body >
+
+< h1 > Ninh Khanh Ly 2021050425!</>
+
+<p>This page has a light blue background color!</p>
+
+</body>
+</html>
+
+
