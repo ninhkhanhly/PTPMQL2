687ff660a2b896790696dbd2f28f2085b0cb26ea
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-09-16 01:10:28 +0700

    làm d

---
+++ CSS/HowToAddCSS.cs
@@ -0,0 +1,14 @@
+< !DOCTYPE html >
+< html >
+< head >
+< link rel = "stylesheet" href = "mystyle.css" >
+</ head >
+< body >
+
+< h1 > Ninh Khanh Ly 2021050425</>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
+
+
